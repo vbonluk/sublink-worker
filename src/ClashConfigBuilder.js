39ac89d5b7453fa3bfbb0449003142318bcb3057
Vbon <5914018@qq.com>
2025-07-22 13:50:08 +0800

    更新 ClashConfigBuilder.js

--- src/ClashConfigBuilder.js
+++ src/ClashConfigBuilder.js
@@ -271,9 +271,9 @@ export class ClashConfigBuilder extends BaseConfigBuilder {
 
         this.config.rules = [...ruleResults]
 
-        this.config.rules.push(`DOMAIN-SUFFIX,google.com,${t('outboundNames.TestFairy Auto')}`);
-        this.config.rules.push(`DOMAIN-SUFFIX,testfairy.com,${t('outboundNames.TestFairy Auto')}`);
-        this.config.rules.push(`DOMAIN-SUFFIX,amazonaws.com,${t('outboundNames.TestFairy Auto')}`);
+        this.config.rules.push(`DOMAIN-SUFFIX,google.com,${t('outboundNames.Auto Select')}`);
+        this.config.rules.push(`DOMAIN-SUFFIX,testfairy.com,${t('outboundNames.Auto Select')}`);
+        this.config.rules.push(`DOMAIN-SUFFIX,amazonaws.com,${t('outboundNames.Auto Select')}`);
         this.config.rules.push(`MATCH,DIRECT`);
 
         return yaml.dump(this.config);
